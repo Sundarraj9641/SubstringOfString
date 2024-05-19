0211f90fdb79527477c784880b190a33fce53157
Mahalingam <sundarraj.mahalingam@capgemini.com>
2024-05-19 10:54:22 +0530

    pushed by sundarraj

---
+++ src/com/substring/SubString.java
@@ -0,0 +1,16 @@
+package com.substring;
+
+public class SubString {
+	public static void main(String[] args) {
+		
+		String str1 = "hello sundarraj";
+		
+		String str2 = str1.substring(6);
+		
+		String str3 = str1.substring(0,5);
+		
+		System.out.println(str2);
+		System.out.println(str3);
+	}
+
+}
